b0a5d368b48d58b5c065a81d5dca7f5a6b881f87
robertmackness <robert.mackness.gbr@gmail.com>
2015-11-02 11:19:48 +0000

    Initial Commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+App = React.createClass ( {
+
+// Mixins
+// For React accessing data from MongoDB via Meteor
+  mixins: [ReactMeteorData]
+
+  render() {
+    
+  }
+
+});
---
+++ Dashboard.jsx
@@ -0,0 +1,21 @@
+ChatClientDashboard = React.createClass({
+  
+  render() {
+    // Render Dashboard components
+    return (
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-2 col-md-2">
+          </div>
+          <div class="col-sm-3 col-md-3">
+          </div>
+          <div class="col-sm-5 col-md-5">
+          </div>
+          <div class="col-sm-2 col-md-2">
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+});
